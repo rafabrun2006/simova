b6bd6d882eb9fb4b12bc3d3eb6ac6e2c7942ff88
Hebert <hebert.hmeso@gmail.com>
2013-03-05 00:42:50 -0300

    Organizando projeto

--- application/modules/site/controllers/IndexController.php
+++ application/modules/site/controllers/IndexController.php
@@ -1,18 +1,72 @@
 <?php
 
-class Site_IndexController extends Zend_Controller_Action
-{
+class Site_IndexController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        echo APPLICATION_PATH;
+    public function indexAction() {
+        
     }
 
+    public function institucionalAction() {
+        
+    }
+
+    public function consultarVacinasAction() {
+        
+    }
+
+    public function consultarCalendarioAction() {
+        
+    }
+
+    public function centroSaudeAction() {
+        
+    }
+
+    public function consultarLegislacaoAction() {
+        
+    }
+
+    public function campanhasAction() {
+        
+    }
+
+    public function faleConoscoAction() {
+
+        require APPLICATION_PATH . '/modules/site/forms/FaleConosco.php';
+
+        $form = new Site_Form_FaleConosco();
+
+
+        if ($this->_request->isPost()) {
+            # RECEBENDO OS DADOS DO FORMULÁRIO
+
+            $post = $this->_request->getPost();
+            $nome = $post["nome"];
+            $email = $post["email"];
+            $conteudo = $post["conteudo"];
+            
+            global $email;
+
+
+
+        mail("itgills@gmail.com", "$assunto", "Nome: $nome\nData: $data\nIp: $ip\nNavegador: $navegador\nHora: $hora\nSite: $site\nE-mail: $email\nTelefone: $fone\n\nMensagem: $mensagem", "From: $email"
+        );
+
+        $site = "itgills@gmail.com";
+        $titulo = "Resposta automatica";
+        $msg = "$nome, obrigado por entrar em contato conosco, em breve entraremos em contato";
+
+        mail("$email", "$titulo", "$msg", "From: $site");
+        
+        }
+        
+        $this->view->form = $form;
+       
+    }
 
 }
 
--- application/modules/site/forms/FaleConosco.php
+++ application/modules/site/forms/FaleConosco.php
@@ -1,15 +1,24 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+class Site_Form_FaleConosco extends Zend_Form
+{
 
-/**
- * Description of CadastroVacinas
- *
- * @author bruno
- */
-class Site_Form_FaleConosco extends Zend_Form {
-    //put your code here
+    public function init()
+    {
+       $nome= new Zend_Form_Element_Text("nome",array("label"=>"Nome: "));
+       $this->addElement($nome);
+       
+       
+       $email= new Zend_Form_Element_Text("email", array("label"=>"E-mail: "));
+       $this->addElement($email);
+       
+       
+       $conteudo= new Zend_Form_Element_Textarea("conteudo",array("label"=>"Conteudo: " ,"cols"=>"40", "rows"=>"2"));
+       $this->addElement($conteudo);
+       
+       
+    }
+
+
 }
+
