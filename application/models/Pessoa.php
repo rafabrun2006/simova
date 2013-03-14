bf958cb259f2fce5ed5922522e87465926eb1622
Rafael Bruno <rafabrun2006@gmail.com>
2013-03-13 22:40:37 -0300

    Trabalhando formulario de cadastro de pessoa

---
+++ application/models/Pessoa.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of Pessoa
+ *
+ * @author bruno
+ */
+class Model_Pessoa extends Zend_Db_Table_Abstract {
+    
+    protected $_name = 'tb_pessoa';
+    protected $_primary = 'cod_pessoa';
+    
+}
