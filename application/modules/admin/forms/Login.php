a708ac7097696b0fd471b71563b297c2988fa811
Rafael Bruno <rafabrun2006@gmail.com>
2013-03-17 19:40:55 -0300

    Add, form login.

---
+++ application/modules/admin/forms/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Perfil
+ *
+ * @author bruno
+ */
+class Admin_Form_Login extends Zend_Form {
+
+    public function init() {
+
+        $cod_login = new Zend_Form_Element_Hidden('cod_login');
+        $nome_login = new Zend_Form_Element_Text('nome_login');
+        $senha_login = new Zend_Form_Element_Text('senha_login');
+
+        $this->addElements(array(
+            $cod_login, $nome_login, $senha_login)
+        );
+
+        foreach ($this->getElements() as $element) {
+            $element->removeDecorator('HtmlTag')->removeDecorator('Label');
+        }
+    }
+
+}
