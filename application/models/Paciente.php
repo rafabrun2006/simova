06f3026f852633c1fdaa01fce39c858c4d219105
Rafael Bruno <rafabrun2006@gmail.com>
2013-03-19 03:50:16 -0300

    Versão com aplicação dos conceitos de acessibilidade e usabilidade e inicio de trabalho com formulario de paciente.
    Boa noite para todos!

---
+++ application/models/Paciente.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of Pessoa
+ *
+ * @author bruno
+ */
+class Model_Paciente extends Simova_Mapper_ModelMapper {
+
+    protected $_name = 'tb_paciente';
+    protected $_primary = 'cod_paciente';
+
+}
