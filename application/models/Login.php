aec39c5720d0c1e44ed13fde27dc157490a61800
Rafael Bruno <rafabrun2006@gmail.com>
2013-03-24 23:50:37 -0300

    Correções de segurança.

--- application/models/Login.php
+++ application/models/Login.php
@@ -9,10 +9,14 @@ class App_Model_Login extends Simova_Mapper_ModelMapper {
     
     protected $_name = 'tb_login';
     protected $_primary = 'cod_login';
-    
+
     public function saveLogin($data){
-        $data['senha_login'] = md5($data['senha_login']);
+        $data['senha_login'] = $this->encodingMd5($data['senha_login']);
         return $this->save($data);
+    }
+    
+    protected function encodingMd5($value){
+        return md5($value);
     }
     
 }
