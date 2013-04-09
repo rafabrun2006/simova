33f487025bdeeee24638be68729aa681f5ab53c4
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-09 01:55:16 -0300

    Add controller para cadastro de vacinas.

--- application/modules/admin/controllers/VacinaController.php
+++ application/modules/admin/controllers/VacinaController.php
@@ -1,26 +1,27 @@
 <?php
 
-class Admin_VacinaController extends Zend_Controller_Action
-{
+class Admin_VacinaController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        // action body
-    }
-    public function consultaVacinaAction()
-    {
-        // action body
-    }
-    public function cadastroVacinaAction()
-    {
+    public function indexAction() {
         // action body
     }
 
+    public function consultaVacinaAction() {
+        $model = new App_Model_Vacina();
+    }
+
+    public function cadastroVacinaAction() {
+        $form = new Admin_Form_LoteVacina();
+        
+        $model = new App_Model_Vacina();
+        $modelLote = new App_Model_Lote();
+
+        $this->view->form = $form;
+    }
 
 }
 
