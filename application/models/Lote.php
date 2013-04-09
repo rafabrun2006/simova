822d01af1773d1597e31439381c1399692088406
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-09 01:56:34 -0300

    Add models lote a lote vacina para cadastro de vacinas.

---
+++ application/models/Lote.php
@@ -0,0 +1,10 @@
+<?php
+
+class App_Model_Lote extends Simova_Mapper_ModelMapper
+{
+    
+    protected $_name = 'tb_lote';
+    protected $_primary = 'cod_lote';
+
+}
+
---
+++ application/models/LoteVacina.php
@@ -0,0 +1,10 @@
+<?php
+
+class App_Model_LoteVacina extends Simova_Mapper_ModelMapper
+{
+    
+    protected $_name = 'tb_lote_vacina';
+    protected $_primary = 'cod_lote_vacina';
+
+}
+
