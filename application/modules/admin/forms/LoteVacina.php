1bdf04e0968125c2ee8be6764637f8f39fa490c6
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-09 01:54:39 -0300

    Add formularios para cadastro de vacinas.

---
+++ application/modules/admin/forms/LoteVacina.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Vacina
+ *
+ * @author bruno
+ */
+class Admin_Form_LoteVacina extends Zend_Form {
+
+    public function init() {
+
+        $codLoteVacina = new Zend_Form_Element_Hidden('cod_lote_vacina');
+        $qtdVacina = new Zend_Form_Element_Text('qtd_vacina');
+
+        $this->addElements(array($codLoteVacina, $qtdVacina));
+        
+        $this->addSubForm(new Admin_Form_Vacina(), 'Vacina');
+        $this->addSubForm(new Admin_Form_Lote(), 'Lote');
+        
+        foreach($this->getElements() as $element){
+            $element->removeDecorator('htmlTag')
+                    ->removeDecorator('label');
+        }
+    }
+
+}
