7a5949f0a297cbab0d6c618c3db00819ad0da7a4
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-12 23:00:13 -0300

    Atualização para envio.

--- application/models/Lote.php
+++ application/models/Lote.php
@@ -5,6 +5,14 @@ class App_Model_Lote extends Simova_Mapper_ModelMapper
     
     protected $_name = 'tb_lote';
     protected $_primary = 'cod_lote';
+    
+    public function save($data){
+        
+        $data['dt_venc'] = date('Y/m/d', strtotime($data['dt_venc']));
+        $data['dt_fabric'] = date('Y/m/d', strtotime($data['dt_fabric']));
+        
+        return parent::save($data);
+    }
 
 }
 
