ff074a964536353c5d1abe5e60dd69ad0b66ce00
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-14 22:04:55 -0300

    Alteração para melhoria.

--- application/modules/admin/views/helpers/Pagination.php
+++ application/modules/admin/views/helpers/Pagination.php
@@ -9,37 +9,37 @@ class Zend_View_Helper_Pagination extends Zend_View_Helper_Abstract {
     private $count = null;
 
     public function pagination($dataArray, $currentPage, $linkNumber) {
-        
+
         $std = new stdClass();
-        
+
         $paginator = Zend_Paginator::factory($dataArray);
-        
+
         $paginator
                 ->setCurrentPageNumber($currentPage)
                 ->setItemCountPerPage(2)
                 ->setPageRange(2);
-        
+
         $std->paginator = $paginator;
-        
+
         /*
          * Definindo variavel que contem numeros com links para paginação
          */
         $template = "<li><a href='%s'>%s</a></li>";
         $pagesInRange = $paginator->getPages();
-        
-        foreach($pagesInRange->pagesInRange as $page){
-            $numberPage .= sprintf($template, $linkNumber.$page, $page);
+
+        foreach ($pagesInRange->pagesInRange as $page) {
+            $numberPage .= sprintf($template, $linkNumber . $page, $page);
         }
-        
+
         $std->pages = $numberPage;
-        
+
         /*
          * Definindo variavel que contem paginação Next e Previous
          */
-        $std->next = $paginator->getCurrentPageNumber()+1;
-        $std->previous = $paginator->getCurrentPageNumber()-1;
+        $std->next = $linkNumber + $paginator->getCurrentPageNumber() + 1;
+        $std->previous = $linkNumber + $paginator->getCurrentPageNumber() - 1;
         $std->count = $paginator->count();
-        
+
         return $std;
     }
 
