64755377b9e76ec2c80ec374213e7115f1e38c55
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-14 23:23:29 -0300

    Concluida paginação de vacinas.

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -9,22 +9,5 @@ $(document).ready(function() {
     $('.date-utils').datepicker({
         format: 'dd-mm-yyyy'
     });
-
-    $('.pagination').smartpaginator({
-        totalrecords: 100,
-        recordspage: 10,
-        initval: 0,
-        next: 'Proximo',
-        prev: 'Anterior',
-        first: 'Primeiro',
-        last: 'Ultimo',
-        theme: 'green',
-        onchange: 'onChange'
-    });
-
-    function onChange(newPageValue) {
-        alert(newPageValue);
-    }
+    
 });
-
-
