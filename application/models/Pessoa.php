8b6971e4994f6f2ae9d6b41c2d0bc1da398e2f07
Rafael Bruno <rafabrun2006@gmail.com>
2013-04-22 23:32:29 -0300

    Criaçao metodo consulta pessoa completo

--- application/models/Pessoa.php
+++ application/models/Pessoa.php
@@ -33,6 +33,8 @@ class App_Model_Pessoa extends Simova_Mapper_ModelMapper {
         
         $query = $this->select()
                 ->from(array('p' => 'tb_pessoa'), array('*'))
+                ->joinleft(array('f' => 'tb_funcionario'), 'p.cod_pessoa = f.cod_pessoa', array('*'))
+                ->joinleft(array('pc' => 'tb_paciente'), 'p.cod_pessoa = pc.cod_pessoa', array('*'))
                 ->join(array('e' => 'tb_endereco'), 'p.cod_end = e.cod_end', array('*'))
                 ->join(array('t' => 'tb_telefone'), 't.cod_pessoa = p.cod_pessoa', array('*'))
                 ->joinLeft(array('l' => 'tb_login'), 'l.cod_login = p.cod_login', array('*'))
