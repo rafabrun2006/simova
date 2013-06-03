1276b544d78893591924f061afb7b5383851ef63
Rosete <rosetebsb@gmail.com>
2013-06-02 23:33:54 -0300

    Add Banner

---
+++ public/banners/administrator/js/pessoa.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+
+$(document).ready(function() {
+
+    $('#dt_nasc').datepicker();
+    $('.date-utils').datepicker();
+
+    $('#cpf').mask('999.999.999-99');
+
+    //$('#rg').mask('9.999.999');
+
+    $('#cep').mask('99.999-999');
+
+    $('#telefone_residencial').mask('(99)9999-9999');
+
+    $('#telefone_celular').mask('(99)9999-9999');
+
+    $('#tipoConsulta').change(function() {
+
+        if ($(this).val() == 'cod_sit') {
+            $('#div-search-select').attr('class', 'hidden-print');
+            $('#div-search-input').attr('class', 'hidden');
+            $('#appendedInputButton').attr('name', 'disable');
+        } else {
+            $('#div-search-select').attr('class', 'hidden');
+            $('#div-search-input').attr('class', 'hidden-print');
+            $('#appendedInputButton').attr('name', 'search');
+        }
+    });
+
+});
+
+
