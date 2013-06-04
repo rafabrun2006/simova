6547cd1e216fd38c7628d87e6b08e73673d6da56
Rafael Bruno <rafabrun2006@gmail.com>
2013-06-04 03:48:12 -0300

    Correções em login e estado

--- application/models/Funcionario.php
+++ application/models/Funcionario.php
@@ -35,6 +35,8 @@ class App_Model_Funcionario extends Simova_Mapper_ModelMapper {
                 ->from(array('f' => 'tb_funcionario'), array('*'))
                 ->join(array('p' => 'tb_pessoa'), 'f.cod_pessoa = p.cod_pessoa', array('*'))
                 ->join(array('e' => 'tb_endereco'), 'p.cod_end = e.cod_end', array('*'))
+                ->join(array('c' => 'tb_cidade'), 'e.cod_cid = c.cod_cid', array('*'))
+                ->join(array('est' => 'tb_uf'), 'c.cod_uf = est.cod_uf', array('*'))
                 ->joinLeft(array('l' => 'tb_login'), 'l.cod_login = p.cod_login', array('*'))
                 ->where('p.cod_pessoa = ?', $cod_pessoa)
                 ->setIntegrityCheck(false)
